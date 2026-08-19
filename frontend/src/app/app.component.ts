08381b24bb33b52344088e5ba737d126bfe944a3
kgudiel-2025180 <kgudiel-2025180@kinal.edu.gt>
2026-08-18 22:13:51 -0600

    feat: se integro componente de sesion expirada en el AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
+import { SessionExpiredComponent } from './shared/components/session-expired/session-expired.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SessionExpiredComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.authService.checkSession();
+  }
+}
